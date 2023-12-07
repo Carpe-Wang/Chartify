8cb98427569c510f0b6cffc991bd18676d5a115c
Carpe-Wang <wangcarpe@126.com>
2023-12-07 20:05:10 +0800

    first commit

---
+++ src/main/java/com/chartify/chartify/ChartifyApplication.java
@@ -0,0 +1,13 @@
+package com.chartify.chartify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChartifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chartify/chartify/ChartifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chartify.chartify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
