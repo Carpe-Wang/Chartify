fd6f1236a9e93de9247036fc88540077c11e3710
Carpe-Wang <wangcarpe@126.com>
2024-01-02 20:37:41 +0800

    完善一个逻辑

--- src/main/java/com/chartify/chartify/service/databaseService/DatabaseService.java
+++ src/main/java/com/chartify/chartify/service/databaseService/DatabaseService.java
@@ -1,11 +1,13 @@
 package com.chartify.chartify.service.databaseService;
 
+import com.chartify.chartify.entity.sqlEntity.DatabaseCredentials;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 public interface DatabaseService {
-    Connection connect(String address, String username, String password) throws SQLException;
+    Connection connect(DatabaseCredentials databaseCredentials) throws SQLException;
     List<Map<String, Object>> executeQuery(Connection conn, String query) throws SQLException;
 }
